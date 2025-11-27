d57771e2a9b78d178567f23becbfc4fe4c4753f6
proccaHumber <236927188+proccaHumber@users.noreply.github.com>
2025-11-26 21:53:58 -0500

    Generating current year on footer copyright notice

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
-const app = {}
+const app = {};
+
+app.currDate = new Date();
+app.currYear = app.currDate.getFullYear();
+
+app.postFooter = () => {
+  app.footerText = document.querySelector(".footerCopy")
+  app.footerText.textContent = `Copyright © ${app.currYear} MyRealCompany Inc LLC United`
+}
 
 app.init = () => {
-
+  app.postFooter()
 }
 
 if (document.readyState === 'loading') {
